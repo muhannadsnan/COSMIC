db45a230222cbd16fd882d73b863477a2b36dfa8
muhannadsnan <msn-23@live.com>
2018-12-04 23:27:37 +0100

    Design established with partials and master page

---
+++ resources/views/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('partials.head')
+</head>
+<body>
+    <div id="app">
+        @include('partials.nav')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
