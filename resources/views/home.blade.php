38b124d312879a2658336b3be34229324f958ea3
muhannadsnan <msn-23@live.com>
2018-12-07 20:25:20 +0100

    design fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <h4 class="card-header bg-primary text-lg">Dashboard</h4>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!
+            @auth
+                <div class="card">
+                    <h4 class="card-header bg-primary text-lg">Dashboard</h4>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif     
+                        You are logged in!
+                    </div>
                 </div>
-            </div>
+            @endauth
+            @guest
+                <div class="card">
+                    <h4 class="card-header bg-warning text-lg">Dashboard</h4>
+                    <div class="card-body">
+                        This is the Welcome page for guests.            
+                    </div>
+                </div>
+            @endguest
         </div>
     </div>
 </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -10,7 +10,15 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('item1') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('item2') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('item3') }}</a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
